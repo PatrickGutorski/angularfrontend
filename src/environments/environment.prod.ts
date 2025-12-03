0744c20584bc28a85283467578943d618e0e8406
patrick gutorski <patrickgutorski@gmail.com>
2025-12-03 09:35:49 +0100

    deploy v8 amd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // OLD: apiUrl: 'http://57.153.167.65'
-  // NEW: Just use /api. The browser automatically appends the current domain.
   apiUrl: '/api'
 };
